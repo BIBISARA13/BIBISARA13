dd4b5e1a4bebd2e0e8112c7533dc7f2b103d79c1
BIBISARA13 <hajirasara08@gmail.com>
2024-11-26 13:01:23 +0530

    Update Oddoreven.C

--- Oddoreven.C
+++ Oddoreven.C
@@ -1,1 +1,12 @@
+#include< stdio.h>
+ int main () {
+Printf:("enter the number");
+scanf("%d", & sum");
+if (num % 2==0) {
+Printf("the number is even \n");
+} else (
+Printf("the number is odd \n");
+}
+return 0
+}
 
